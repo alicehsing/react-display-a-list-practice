ff856be5430dfeb25438fa583128aff86693f93d
Alice Hsing <alicehsing@gmail.com>
2022-01-27 16:46:18 -0800

    test passed

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders candies', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/candies/i);
   expect(linkElement).toBeInTheDocument();
 });
